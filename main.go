5325c06a33d108a1a5a55ba09a61a289496a14c1
liguolin <liguolin@>
2019-11-30 11:04:38 +0800

    init

--- main.go
+++ main.go
@@ -9,34 +9,12 @@ import (
 	"strconv"
 )
 
-var users = make(map[int]User)
-
-func init() {
-	user := User{
+var users = map[int]User{
+	1: User{
 		Id:   1,
 		Name: "小明",
 		Age:  10,
-	}
-	users[user.Id] = user
-}
-
-func main() {
-
-	http.HandleFunc("/", hello)
-	http.HandleFunc("/user", getUser)
-	//GET localhost:9090/save/user?id=3&name=bazinga&age=21
-	//POST localhost:9090/save/user
-	//{
-	//	"Id": 4,
-	//	"Name": "jarvis",
-	//	"Age": 22
-	//}
-	http.HandleFunc("/save/user", saveUser)
-	err := http.ListenAndServe(":9090", nil)
-	if err != nil {
-		log.Fatal("ListenerAndServe:", err)
-		return
-	}
+	},
 }
 
 type User struct {
@@ -45,7 +23,75 @@ type User struct {
 	Age  int
 }
 
+//GET localhost:9090/save/user?id=3&name=bazinga&age=21
+//POST localhost:9090/save/user
+//{
+////	"Id": 4,
+////	"Name": "jarvis",
+////	"Age": 22
+////}
+func main() {
+
+	http.HandleFunc("/user", getUser)
+	http.HandleFunc("/save/user", saveUser)
+	http.HandleFunc("/update/user", updateUser)
+	http.HandleFunc("/delete/user", deleteUser)
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		log.Fatal("ListenerAndServe:", err)
+		return
+	}
+}
+
+func deleteUser(writer http.ResponseWriter, request *http.Request) {
+
+	params := request.URL.Query()
+	idStr := params["id"][0]
+	id, _ := strconv.Atoi(idStr)
+
+	delete(users, id)
+
+	fmt.Fprintln(writer, "delete success")
+	return
+
+}
+
+func updateUser(writer http.ResponseWriter, request *http.Request) {
+
+	if request.Method == "POST" {
+		bytes, _ := ioutil.ReadAll(request.Body)
+		updateUser := User{}
+		err := json.Unmarshal(bytes, &updateUser)
+
+		if err != nil {
+			panic("param is err")
+		}
+		_, ok := users[updateUser.Id]
+		if ok {
+			users[updateUser.Id] = updateUser
+			fmt.Fprintln(writer, "update success")
+		} else {
+			fmt.Fprintln(writer, "user not exist not need update")
+		}
+		return
+	}
+
+}
+
 func saveUser(writer http.ResponseWriter, request *http.Request) {
+
+	if request.Method == "POST" {
+		bytes, _ := ioutil.ReadAll(request.Body)
+		saveUser := User{}
+		err := json.Unmarshal(bytes, &saveUser)
+
+		if err != nil {
+			panic("param is err")
+		}
+		users[saveUser.Id] = saveUser
+		fmt.Fprintln(writer, "success")
+		return
+	}
 
 	if request.Method == "GET" {
 		params := request.URL.Query()
@@ -61,19 +107,6 @@ func saveUser(writer http.ResponseWriter, request *http.Request) {
 			Age:  age,
 		}
 		users[id] = newUser
-		fmt.Fprintln(writer, "success")
-		return
-	}
-
-	if request.Method == "POST" {
-		bytes, _ := ioutil.ReadAll(request.Body)
-		saveUser := User{}
-		err := json.Unmarshal(bytes, &saveUser)
-
-		if err != nil {
-			panic("param is err")
-		}
-		users[saveUser.Id] = saveUser
 		fmt.Fprintln(writer, "success")
 		return
 	}
