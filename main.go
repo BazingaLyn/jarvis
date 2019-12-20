0cc4a9517fc694795e5bc1b89e11c166aa606c9f
11108118 <liguolin@vivo.com>
2019-12-20 09:29:00 +0800

    init

--- main.go
+++ main.go
@@ -5,6 +5,13 @@ import (
 	"github.com/BazingaLyn/jarvis/routers"
 )
 
+// @title gin实战
+// @version 1.0
+// @description gin开发实战接口列表
+
+// @contact.name API Support
+// @host 127.0.0.1:8080
+// @BasePath
 func main() {
 	r := routers.Routers()
 	r.Run()
